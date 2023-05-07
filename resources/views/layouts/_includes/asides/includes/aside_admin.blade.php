391d6bcd31b5368d7a339c8194bf20d416f4344d
Monteiro Futila <jonquerfutila@gmail.com>
2023-05-07 14:34:27 +0100

    Desenhando o front do sistema

--- resources/views/layouts/_includes/asides/includes/aside_admin.blade.php
+++ resources/views/layouts/_includes/asides/includes/aside_admin.blade.php
@@ -55,26 +55,26 @@
 <li class="nav-item">
     <a href="projects.html">
         <i class="fas fa-cogs"></i>
-        <p>Entidade</p>
+        <p>Dados do mercado</p>
     </a>
 </li>
 
 <li class="nav-item">
     <a data-toggle="collapse" href="#workers">
         <i class="fas fa-user-tie"></i>
-        <p>Trabalhadores</p>
+        <p>Vendedores</p>
         <span class="caret"></span>
     </a>
     <div class="collapse" id="workers">
         <ul class="nav nav-collapse">
             <li>
                 <a href="{{route('workers.create')}}">
-                    <span class="sub-item">Novo</span>
+                    <span class="sub-item">Cadastrar vendedor</span>
                 </a>
             </li>
             <li>
                 <a href="{{route('workers.index')}}">
-                    <span class="sub-item">Lista</span>
+                    <span class="sub-item">Lista de vendedores</span>
                 </a>
             </li>
         </ul>
@@ -90,12 +90,12 @@
         <ul class="nav nav-collapse">
             <li>
                 <a href="{{route('users.create')}}">
-                    <span class="sub-item">Novo</span>
+                    <span class="sub-item">Cadastrar usuário</span>
                 </a>
             </li>
             <li>
                 <a href="{{route('users.index')}}">
-                    <span class="sub-item">Lista</span>
+                    <span class="sub-item">Lista de usuários</span>
                 </a>
             </li>
         </ul>
